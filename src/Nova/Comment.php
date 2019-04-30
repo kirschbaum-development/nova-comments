e2dd8a265322a4ca7a8b9686abbf48b459dc2194
Brandon Ferens <brandonferens@gmail.com>
2019-04-30 21:40:16 +0000

    Apply fixes from StyleCI

--- src/Nova/Comment.php
+++ src/Nova/Comment.php
@@ -41,6 +41,7 @@ class Comment extends Resource
      * Get the fields displayed by the resource.
      *
      * @param  \Illuminate\Http\Request  $request
+     *
      * @return array
      */
     public function fields(Request $request)
@@ -72,6 +73,7 @@ class Comment extends Resource
      * Get the cards available for the request.
      *
      * @param  \Illuminate\Http\Request  $request
+     *
      * @return array
      */
     public function cards(Request $request)
@@ -83,6 +85,7 @@ class Comment extends Resource
      * Get the filters available for the resource.
      *
      * @param  \Illuminate\Http\Request  $request
+     *
      * @return array
      */
     public function filters(Request $request)
@@ -94,6 +97,7 @@ class Comment extends Resource
      * Get the lenses available for the resource.
      *
      * @param  \Illuminate\Http\Request  $request
+     *
      * @return array
      */
     public function lenses(Request $request)
@@ -105,6 +109,7 @@ class Comment extends Resource
      * Get the actions available for the resource.
      *
      * @param  \Illuminate\Http\Request  $request
+     *
      * @return array
      */
     public function actions(Request $request)
